77a9311d56116a14106ee1d081a54f583778966e
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-29 14:51:16 -0300

    feat: route to create an account

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import express from "express";
+import { randomUUID } from "node:crypto";
 
 const app = express();
+
+app.use(express.json());
+
+const customers = [];
+
+// Criação de conta
+app.post("/account", (request, response) => {
+  const { name, CPF } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.CPF === CPF
+  );
+
+  if (customerAlreadyExists)
+    return response.status(400).json({ error: "Customer already exists!" });
+
+  customers.push({
+    id: randomUUID(),
+    name,
+    CPF,
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
